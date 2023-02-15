4b008ec5ba9c313c0e1633993d463c304493d929
MonaPatel1223 <minalthakor1223@gmail.com>
2023-02-15 01:42:53 +0000

    javafrist

---
+++ JavaTopic.java
@@ -0,0 +1,13 @@
+package java1;
+/* Change the packge name java to java1*/
+public class JavaTopic {
+    public static void main(String[] args)
+    {
+        System.out.println("What is class");
+        System.out.println("what is object");
+        System.out.println("what is variable");
+        System.out.println("what is loops");
+        System.out.println("what is Method");
+
+    }
+}
